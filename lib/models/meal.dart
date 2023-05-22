7c283e7f32883c377535baceb020cc7e227d3892
Kaio Goes <kaio.goes@spezi.com.br>
2023-05-21 23:09:41 -0300

    Feature finish meal item

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -21,18 +21,45 @@ class Meal {
   final Complexity complexity;
   final Cost cost;
 
-  const Meal(
-      {required this.id,
-      required this.categories,
-      required this.title,
-      required this.imageUrl,
-      required this.ingredients,
-      required this.steps,
-      required this.duration,
-      required this.isGlutenFree,
-      required this.isLactoseFree,
-      required this.isVegan,
-      required this.isVegetarian,
-      required this.complexity,
-      required this.cost});
+  const Meal({
+    required this.id,
+    required this.categories,
+    required this.title,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.steps,
+    required this.duration,
+    required this.isGlutenFree,
+    required this.isLactoseFree,
+    required this.isVegan,
+    required this.isVegetarian,
+    required this.complexity,
+    required this.cost,
+  });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return 'Simples';
+      case Complexity.medium:
+        return 'Normal';
+      case Complexity.difficult:
+        return 'Difícil';
+      default:
+        return 'Desconhecida';
+    }
+  }
+
+   String get costText {
+    switch (cost) {
+      case Cost.cheap:
+        return 'Barato';
+      case Cost.fair:
+        return 'Justo';
+      case Cost.expensive:
+        return 'Caro';
+      default:
+        return 'Desconhecido';
+    }
+  }
 }
